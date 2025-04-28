814221e7a4966620fd47c12bf9d6f4f691b37fa8
omarawel <166297090+omarawel@users.noreply.github.com>
2025-04-28 23:58:03 +0200

    Create products.js

---
+++ server/src/routes/products.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Product = require('../models/Product');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Fehler beim Laden der Produkte' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: 'Fehler beim Erstellen des Produkts' });
+  }
+});
+
+module.exports = router;
